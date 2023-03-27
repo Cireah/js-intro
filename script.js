31d3c3386e8db6adf07e57e76cd3a7da0ede97be
simpan824 <simon.dagernas@elev.ga.ntig.se>
2023-03-27 09:19:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const country = "sweden";
+const continent = "europe";
+let population = "10 000 000";
+const isLand = true;
+let language = "swedish";
+
+console.log(country + "\n" + continent + "\n" + population);
+console.log("Is it a land? " + isLand + "\n" + language);
