94d564defc27108f18233c5057844dd5620fc013
simpan824 <simon.dagernas@elev.ga.ntig.se>
2023-03-27 09:30:44 +0200

    woah

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+/*uppgift 1 svar:
+const country = "sweden";
+const continent = "europe";
+let population = "10 000 000";
+console.log(country + "\n" + continent + "\n" + population);*/
+
+/*uppgift 2 svar:
+kolla på index.html så kan man se att det är länkat*/
+
+/*uppgift 3 svar:
 const country = "sweden";
 const continent = "europe";
 let population = "10 000 000";
 const isLand = true;
-let language = "swedish";
+let language;
 
-console.log(country + "\n" + continent + "\n" + population);
-console.log("Is it a land? " + isLand + "\n" + language);
+population = 10;
+
+console.log(country + "\n" + continent + "\n" + population + "\n" + "Is it a land? " + isLand + "\n" + language);
+*/
+
+//uppgift 4 svar:
+const country = "sweden";
+const continent = "europe";
+let population = "10 000 000";
+const isLand = true;
+let language;
+
+population = 10;
+language = "swedish";
+
+console.log(country + "\n" + continent + "\n" + population + "\n" + "Is it a land? " + isLand + "\n" + language);
