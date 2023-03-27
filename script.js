8543a6ff5c3090b11ca1e1d14ca3abf340b4af65
simpan824 <simon.dagernas@elev.ga.ntig.se>
2023-03-27 09:40:47 +0200

    woah

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ console.log(country + "\n" + continent + "\n" + population + "\n" + "Is it a lan
 //uppgift 4 svar:
 const country = "sweden";
 const continent = "europe";
-let population = "10 000 000";
+let population = 10000000;
 const isLand = true;
 let language;
 
